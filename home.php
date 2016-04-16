6fef578d8486bc525d6ac47b4ea6915054ac979a
Cole Tucker <cole.tucker@west-mec.org>
2016-04-15 18:07:48 -0700

    Worked on the home page php.

--- home.php
+++ home.php
@@ -1,23 +1,44 @@
 <?php
-$dbh = new PDO('mysql:host=localhost;dbname=subscription_project', 'root', 'root');
-$stmt = $dbh->prepare($query);
-$stmt->execute();
-$result = $stmt->fetchAll();
+require_once('connect.php');
+
+function getProducts($conn) {
+    $sql = 'SELECT * FROM products ORDER BY name';
+    $stmt = $conn->prepare($sql);
+    if ($stmt->execute()) {
+        $products = '<tr>';
+        $counter = 0;
+        while ($row = $stmt->fetch()) {
+            if ($counter == 3) {
+                $counter = 0;
+                $products .= '<tr>';
+            }
+            $products .= '
+            <td><img src="'.$row['image'].'" height="300px" width="300px">
+                <p>'.$row['name'].'</p>
+                <p>'.$row['description'].'</p>
+                <p>'.$row['address'].'</p>
+                <p>$'.$row['price'].'</p>
+                <form method="post" action="shoppingCart.php">
+                <input type="hidden" name="id" value="'.$row['id'].'"/>
+                <input type="submit" name="add" value="ADD"/>
+                </form></td>
+        ';
+        }
+        echo $products;
+    }
+}
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Subscription</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>New York Apartments</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
-<body>
-<h2>Sites to Subscribe to:</h2>
-
-
+<body id="products">
 <div class="aboutUl">
     <ul>
         <li><a href="home.php">Home</a></li>
@@ -26,6 +47,18 @@ $result = $stmt->fetchAll();
     </ul>
 </div>
 
+<h1>Products</h1>
+<div class="content">
+    <div class="productContent">
+        <div class="productTable">
+            <table border="1px">
+                <?php
+                getProducts($dbh);
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
 </body>
 </html>
 
